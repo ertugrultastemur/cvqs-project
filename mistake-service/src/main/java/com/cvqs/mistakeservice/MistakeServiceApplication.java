645248e6ed6446aaf3813734764d2df6bf20589c
Ertuğrul Taştemür <34662874+ertugrultastemur@users.noreply.github.com>
2023-06-19 01:07:44 +0300

    first commit
    mistake-service created.

---
+++ mistake-service/src/main/java/com/cvqs/mistakeservice/MistakeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cvqs.mistakeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MistakeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MistakeServiceApplication.class, args);
+	}
+
+}
---
+++ mistake-service/src/test/java/com/cvqs/mistakeservice/MistakeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cvqs.mistakeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MistakeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
