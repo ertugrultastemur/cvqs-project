b38094851bca06045fc0ec346a1b3c78121ffd84
Ertuğrul Taştemür <34662874+ertugrultastemur@users.noreply.github.com>
2023-06-19 18:07:46 +0300

    UserNotFoundException is updated.
    necessary inheritance has been achieved.
    Ctor is added.

--- user-management-service/src/main/java/com/cvqs/usermanagementservice/exception/UserNotFoundException.java
+++ user-management-service/src/main/java/com/cvqs/usermanagementservice/exception/UserNotFoundException.java
@@ -1,4 +1,13 @@
 package com.cvqs.usermanagementservice.exception;
 
-public class UserNotFoundException {
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class UserNotFoundException extends RuntimeException {
+
+    public UserNotFoundException(String e){
+        super(e);
+    }
 }
