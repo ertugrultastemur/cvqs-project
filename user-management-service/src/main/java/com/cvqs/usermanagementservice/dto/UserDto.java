0c46324fa252c320c82992540b9a70f435b4f9c5
Ertuğrul Taştemür <34662874+ertugrultastemur@users.noreply.github.com>
2023-06-19 17:57:14 +0300

    UserDto is updated.
    convert method is added.

--- user-management-service/src/main/java/com/cvqs/usermanagementservice/dto/UserDto.java
+++ user-management-service/src/main/java/com/cvqs/usermanagementservice/dto/UserDto.java
@@ -1,4 +1,69 @@
 package com.cvqs.usermanagementservice.dto;
 
+import com.cvqs.usermanagementservice.model.User;
+
 public class UserDto {
+
+    private UserIdDto id;
+
+    private String eMail;
+
+    private String password;
+
+    private Boolean isDeleted = Boolean.FALSE;
+
+    public UserDto(UserIdDto id, String eMail, String password, Boolean isDeleted) {
+        this.id = id;
+        this.eMail = eMail;
+        this.password = password;
+        this.isDeleted = isDeleted;
+    }
+
+    public UserDto() {
+    }
+
+    public static UserDto convert(User user){
+        UserIdDto userIdDto=null;
+        if (user.getUserId()!=null){
+            userIdDto = UserIdDto.convert(user.getUserId(), user.getUserName());
+        }
+        return new UserDto(
+                userIdDto,
+                user.geteMail(),
+                user.getPassword(),
+                user.getDeleted()
+        );
+    }
+
+    public UserIdDto getId() {
+        return id;
+    }
+
+    public void setId(UserIdDto id) {
+        this.id = id;
+    }
+
+    public String geteMail() {
+        return eMail;
+    }
+
+    public void seteMail(String eMail) {
+        this.eMail = eMail;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Boolean getDeleted() {
+        return isDeleted;
+    }
+
+    public void setDeleted(Boolean deleted) {
+        isDeleted = deleted;
+    }
 }
