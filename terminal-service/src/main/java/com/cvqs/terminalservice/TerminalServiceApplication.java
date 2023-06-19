0f4accb2fcbe9012a647909f8056f4894d2be14a
Ertuğrul Taştemür <34662874+ertugrultastemur@users.noreply.github.com>
2023-06-19 14:51:44 +0300

    terminal-service created.

---
+++ terminal-service/src/main/java/com/cvqs/terminalservice/TerminalServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cvqs.terminalservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TerminalServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TerminalServiceApplication.class, args);
+	}
+
+}
---
+++ terminal-service/src/test/java/com/cvqs/terminalservice/TerminalServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cvqs.terminalservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TerminalServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
