af0dfe835472b50b3aa9e4876622648104337d56
Ertuğrul Taştemür <34662874+ertugrultastemur@users.noreply.github.com>
2023-06-19 04:12:22 +0300

    mistake-listing-service and layers are created.

---
+++ mistake-listing-service/src/main/java/com/cvqs/mistakelistingservice/MistakeListingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cvqs.mistakelistingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MistakeListingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MistakeListingServiceApplication.class, args);
+	}
+
+}
