f2f1993142964c462981bb2635f7dc41d9cfd9b2
Ertuğrul Taştemür <34662874+ertugrultastemur@users.noreply.github.com>
2023-06-21 23:26:54 +0300

    Update UserNotFoundException.java
    logs added.

--- user-management-service/src/main/java/com/cvqs/usermanagementservice/exception/UserNotFoundException.java
+++ user-management-service/src/main/java/com/cvqs/usermanagementservice/exception/UserNotFoundException.java
@@ -1,13 +1,18 @@
 package com.cvqs.usermanagementservice.exception;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class UserNotFoundException extends RuntimeException {
 
+    Logger logger = LoggerFactory.getLogger(this.getClass());
     public UserNotFoundException(String e){
         super(e);
+        logger.error("User could not found: " + e);
+
     }
 }
