b8e6e1490fdf561b6ad1164cb12cbce333d6c907
Ertuğrul Taştemür <34662874+ertugrultastemur@users.noreply.github.com>
2023-06-21 23:22:22 +0300

    Update UserIdDto.java
    logs added.

--- user-management-service/src/main/java/com/cvqs/usermanagementservice/dto/UserIdDto.java
+++ user-management-service/src/main/java/com/cvqs/usermanagementservice/dto/UserIdDto.java
@@ -1,12 +1,18 @@
 package com.cvqs.usermanagementservice.dto;
 
+import org.slf4j.ILoggerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class UserIdDto {
 
+Logger logger = LoggerFactory.getLogger(this.getClass());
     private Integer userId;
 
     private String userName;
 
     public UserIdDto(Integer userId, String userName) {
+        logger.info("UserIdDto ctor entered.");
         this.userId = userId;
         this.userName = userName;
     }
@@ -22,18 +28,22 @@ public class UserIdDto {
     }
 
     public Integer getUserId() {
+        logger.info("UserIdDto: getUserId entered.");
         return userId;
     }
 
     public void setUserId(Integer userId) {
+        logger.info("UserIdDto: setUserId entered.");
         this.userId = userId;
     }
 
     public String getUserName() {
+        logger.info("UserIdDto: getUserName entered.");
         return userName;
     }
 
     public void setUserName(String userName) {
+        logger.info("UserIdDto: setUserName entered.");
         this.userName = userName;
     }
 }
