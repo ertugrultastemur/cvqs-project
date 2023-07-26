b1fcd1db5a0b0981cc659a91346f7a081cd8867b
Ertuğrul Taştemür <34662874+ertugrultastemur@users.noreply.github.com>
2023-07-26 18:43:23 +0300

    Create Role.java

---
+++ user-management-service/src/main/java/com/cvqs/usermanagementservice/model/Role.java
@@ -0,0 +1,14 @@
+package com.cvqs.usermanagementservice.model;
+
+import jakarta.persistence.*;
+
+/**
+ * Enumeration representing user roles.
+ */
+public enum Role {
+    TEAM_LEADER,
+
+    ADMIN,
+
+    OPERATOR
+}
